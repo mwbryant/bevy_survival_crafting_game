88e0abdb5d97c6447a0d890326ef50b02cbbb328
Matthew <logicprojectsforfpgas@gmail.com>
2022-04-01 10:35:55 -0600

    Basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,130 @@
+#![allow(clippy::redundant_field_names)]
+use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy_inspector_egui::WorldInspectorPlugin;
+
+#[derive(Component)]
+pub struct Player {
+    speed: f32,
+}
+
+pub struct PlaceHolderGraphics {
+    texture_atlas: Handle<TextureAtlas>,
+    player_index: usize,
+    flint_index: usize,
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.3, 0.5, 0.3)))
+        .insert_resource(WindowDescriptor {
+            width: 1600.0,
+            height: 900.0,
+            title: "DST clone".to_string(),
+            vsync: true,
+            resizable: false,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system_to_stage(StartupStage::PreStartup, load_graphics)
+        .add_startup_system(spawn_player)
+        .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_flint)
+        .add_system(player_movement)
+        .add_system(camera_follow)
+        .add_plugin(WorldInspectorPlugin::new())
+        .run();
+}
+
+fn spawn_flint(mut commands: Commands, graphics: Res<PlaceHolderGraphics>) {
+    let mut sprite = TextureAtlasSprite::new(graphics.flint_index);
+    sprite.custom_size = Some(Vec2::splat(0.1));
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            sprite: sprite,
+            texture_atlas: graphics.texture_atlas.clone(),
+            ..Default::default()
+        })
+        .insert(Name::new("Flint"));
+}
+
+fn camera_follow(
+    player_query: Query<&Transform, With<Player>>,
+    mut camera_query: Query<&mut Transform, (With<Camera>, Without<Player>)>,
+) {
+    let player_transform = player_query.single().translation;
+    let mut camera_transform = camera_query.single_mut();
+
+    camera_transform.translation.x = player_transform.x;
+    camera_transform.translation.y = player_transform.y;
+}
+
+fn player_movement(
+    keyboard: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    mut player_query: Query<(&mut Transform, &Player)>,
+) {
+    let (mut player_transform, player) = player_query.single_mut();
+
+    if keyboard.pressed(KeyCode::A) {
+        player_transform.translation.x -= player.speed * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::D) {
+        player_transform.translation.x += player.speed * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::W) {
+        player_transform.translation.y += player.speed * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::S) {
+        player_transform.translation.y -= player.speed * time.delta_seconds();
+    }
+}
+
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = OrthographicCameraBundle::new_2d();
+    camera.orthographic_projection.left = -1.0 * 16.0 / 9.0;
+    camera.orthographic_projection.right = 1.0 * 16.0 / 9.0;
+    camera.orthographic_projection.top = 1.0;
+    camera.orthographic_projection.bottom = -1.0;
+    camera.orthographic_projection.scaling_mode = ScalingMode::None;
+
+    commands.spawn_bundle(camera);
+}
+
+fn spawn_player(mut commands: Commands, graphics: Res<PlaceHolderGraphics>) {
+    let mut sprite = TextureAtlasSprite::new(graphics.player_index);
+    sprite.custom_size = Some(Vec2::splat(0.3));
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            sprite: sprite,
+            texture_atlas: graphics.texture_atlas.clone(),
+            ..Default::default()
+        })
+        .insert(Player { speed: 0.3 })
+        .insert(Name::new("Player"));
+}
+
+fn load_graphics(
+    mut commands: Commands,
+    assets: Res<AssetServer>,
+    mut texture_assets: ResMut<Assets<TextureAtlas>>,
+) {
+    let image_handle = assets.load("placeholder.png");
+    let mut atlas = TextureAtlas::new_empty(image_handle, Vec2::splat(256.0));
+    let player_index = atlas.add_texture(bevy::sprite::Rect {
+        min: Vec2::splat(0.0),
+        max: Vec2::splat(32.0),
+    });
+
+    let flint_index = atlas.add_texture(bevy::sprite::Rect {
+        min: Vec2::new(32.0, 0.0),
+        max: Vec2::new(48.0, 16.0),
+    });
+
+    let atlas_handle = texture_assets.add(atlas);
+
+    commands.insert_resource(PlaceHolderGraphics {
+        texture_atlas: atlas_handle,
+        player_index: player_index,
+        flint_index: flint_index,
+    });
 }
